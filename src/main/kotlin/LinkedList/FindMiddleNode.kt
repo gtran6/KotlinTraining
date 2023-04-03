c675fdd590513bcfc4e1fbd950d85eef834a3f8f
gtran6 <gtran6@depaul.edu>
2023-04-03 18:05:31 -0500

    commit

---
+++ src/main/kotlin/LinkedList/FindMiddleNode.kt
@@ -0,0 +1,11 @@
+package LinkedList
+
+fun findMiddleNode(head: ListNode?): ListNode? {
+    var slow = head
+    var fast = head
+    while (fast?.next != null) {
+        slow = slow?.next
+        fast = fast.next?.next
+    }
+    return slow
+}
