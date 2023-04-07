4300d048e4ef927b08211e3ea4e87494ecf765cb
gtran6 <gtran6@depaul.edu>
2023-04-07 14:36:32 -0500

    commit

--- src/main/kotlin/leetcode/Zoom/ValidateBinarySearchTree.kt
+++ src/main/kotlin/leetcode/Zoom/ValidateBinarySearchTree.kt
@@ -1,2 +1,17 @@
 package leetcode.Zoom
 
+fun inOrderBST(array: IntArray, n: Int): Boolean {
+    if (n == 0 || n == 1) return true
+
+    for (i in 1 until n) {
+        if (array[i-1].compareTo(array[i]) == 1) return false
+    }
+    return true
+
+    //return array.contentEquals(array.sortedArray())
+}
+fun main() {
+    val array = intArrayOf(8,12,15,21,24,32,45)
+    val n = array.size
+    println(inOrderBST(array, n))
+}
