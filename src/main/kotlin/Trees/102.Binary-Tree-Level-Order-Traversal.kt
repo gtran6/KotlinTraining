dddf1d6c4793a59ee5e502e3547e91aead86c3c5
gtran6 <gtran6@depaul.edu>
2023-04-26 13:00:00 -0500

    commit

---
+++ src/main/kotlin/Trees/102.Binary-Tree-Level-Order-Traversal.kt
@@ -0,0 +1,19 @@
+package Trees
+
+// recursion
+fun levelOrder2(root: TreeNode?): List<List<Int>> {
+    val res = mutableListOf<List<Int>>()
+    if (root == null) return res
+    traverse(root, 0, res)
+    return res
+}
+fun traverse(node: TreeNode?, level: Int, res: MutableList<List<Int>>) {
+    if (node == null) return
+    if (level >= res.size) {
+        res.add(mutableListOf(node.`val`))
+    } else {
+        res[level] = res[level] + (node.`val`)
+    }
+    traverse(node.left, level+1, res)
+    traverse(node.right, level+1, res)
+}
