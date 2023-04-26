175fb3792dcd2170dccec51f25135a49238dd899
gtran6 <gtran6@depaul.edu>
2023-04-26 13:12:25 -0500

    commit

--- src/main/kotlin/Trees/102.Binary-Tree-Level-Order-Traversal.kt
+++ src/main/kotlin/Trees/102.Binary-Tree-Level-Order-Traversal.kt
@@ -17,3 +17,29 @@ fun traverse(node: TreeNode?, level: Int, res: MutableList<List<Int>>) {
     traverse(node.left, level+1, res)
     traverse(node.right, level+1, res)
 }
+
+// iterative
+fun levelOrder3(root: TreeNode?): List<List<Int>> {
+    val res = mutableListOf<List<Int>>()
+    if (root == null) return res
+    val stack = ArrayDeque<TreeNode>()
+    stack.add(root)
+
+    while (!stack.isEmpty()) {
+        val levelNodes = mutableListOf<Int>()
+
+        for (i in 0 until stack.size) {
+            val node = stack.removeLast()
+            levelNodes.add(node.`val`)
+
+            if (node.right != null) {
+                stack.add(node.right!!)
+            }
+            if (node.left != null) {
+                stack.add(node.left!!)
+            }
+        }
+        res.add(levelNodes)
+    }
+    return res
+}
