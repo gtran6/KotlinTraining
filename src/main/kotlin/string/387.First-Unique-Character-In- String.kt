3ece917655f3f12aedb724e1f26e66359d237a36
gtran6 <gtran6@depaul.edu>
2023-04-25 23:23:36 -0500

    commit

---
+++ src/main/kotlin/string/387.First-Unique-Character-In- String.kt
@@ -0,0 +1,16 @@
+package string
+
+fun firstUniqChar(s: String): Int {
+    val map = mutableMapOf<Char, Int>()
+    for (c in s) {
+        map[c] = map.getOrDefault(c, 0) + 1
+    }
+    for (i in s.indices) {
+        if (map[s[i]] == 1) return i
+    }
+    return -1
+}
+fun main() {
+    val s = "leetcode"
+    println(firstUniqChar(s))
+}
