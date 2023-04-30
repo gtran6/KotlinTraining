ea557e8fd0ae6281a17adc5e6f9d468114b3eb57
gtran6 <gtran6@depaul.edu>
2023-04-30 13:15:48 -0500

    commit

---
+++ src/main/kotlin/TwoPointers/977.Squares-Of-Sorted-Array.kt
@@ -0,0 +1,19 @@
+package TwoPointers
+
+import kotlin.math.abs
+
+fun sortedSquares1(nums: IntArray): IntArray {
+    val res = IntArray(nums.size)
+    var start = 0
+    var end = nums.size - 1
+    (nums.size - 1 downTo 0).forEach { i ->
+        if (abs(nums[start]) > abs(nums[end])) {
+            res[i] = nums[start] * nums[start]
+            start++
+        } else {
+            res[i] = nums[end] * nums[end]
+            end--
+        }
+    }
+    return res
+}
