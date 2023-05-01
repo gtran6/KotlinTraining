a5ee2f9c75aa33d47e07f1a546c68d711afbbc92
gtran6 <gtran6@depaul.edu>
2023-05-01 17:56:56 -0500

    commit

---
+++ src/main/kotlin/string/MostFrequentCharacter.kt
@@ -0,0 +1,23 @@
+package string
+
+fun mostFrequentChar(s: String) : String {
+    s.lowercase()
+    if (s.isEmpty()) return ""
+    val map = HashMap<Char, Int>()
+    for (c in s) {
+        map[c] = map.getOrDefault(c, 0) + 1
+    }
+    var mostFreqChar = s[0]
+    var max = 0
+    for (i in s.indices) {
+        if (map[s[i]]!! > max || (map[s[i]]!! == max && s[i] < mostFreqChar)) {
+            max = map[s[i]]!!
+            mostFreqChar = s[i]
+        }
+    }
+    return mostFreqChar.toString()
+}
+fun main() {
+    val s = "xyyXzz"
+    println(mostFrequentChar(s))
+}
