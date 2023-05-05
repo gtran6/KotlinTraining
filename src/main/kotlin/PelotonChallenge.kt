2186722ea7882f383e27373644fce0d8a2f06833
gtran6 <gtran6@depaul.edu>
2023-05-05 15:40:02 -0500

    commit

--- src/main/kotlin/PelotonChallenge.kt
+++ src/main/kotlin/PelotonChallenge.kt
@@ -1,59 +1,42 @@
-fun timeInHeartRateZonesForWorkout(
-    timesAndBeats: Array<Pair<Int, Int>>,
-    maxHeartRate: Int,
-    duration: Int
-): Array<Int> {
-    val heartRateZones = listOf(
-        Pair((0.0 * maxHeartRate).toInt(), (0.6 * maxHeartRate).toInt()),
-        Pair((0.6 * maxHeartRate).toInt(), (0.75 * maxHeartRate).toInt()),
-        Pair((0.75 * maxHeartRate).toInt(), (0.85 * maxHeartRate).toInt()),
-        Pair((0.85 * maxHeartRate).toInt(), (1.0 * maxHeartRate).toInt())
+fun timeInHeartRateZonesForWorkout(workout: Array<Pair<Int, Int>>, maxHeartRate: Int): List<Int> {
+    val zones = listOf(
+        0 to 60,
+        60 to 75,
+        75 to 85,
+        85 to 100
     )
+    val timeInZones = MutableList(zones.size) { 0 }
 
-    val zoneDurations = IntArray(4)
+    for (i in 1 until workout.size) {
+        val (prevTime, prevBpm) = workout[i - 1]
+        val (currTime, currBpm) = workout[i]
+        val elapsedTime = currTime - prevTime
+        val avgBpm = (prevBpm + currBpm) / 2.0
+        val percentMax = (avgBpm / maxHeartRate) * 100.0
 
-    var prevTime = 0
-    var prevBpm = timesAndBeats[0].second
-
-    for (i in 1 until timesAndBeats.size) {
-        val time = timesAndBeats[i].first
-        val bpm = timesAndBeats[i].second
-
-        val interval = time - prevTime
-        val prevZone = heartRateZones.lastOrNull { bpm >= it.first } ?: heartRateZones.first()
-
-        val zoneIndex = heartRateZones.indexOf(prevZone)
-        if (zoneIndex >= 0) {
-            zoneDurations[zoneIndex] += interval
+        for (j in zones.indices) {
+            val (minPercent, maxPercent) = zones[j]
+            if (percentMax >= minPercent && percentMax < maxPercent) {
+                timeInZones[j] += elapsedTime
+                break
+            }
         }
-
-        prevTime = time
-        prevBpm = bpm
     }
-
-    val finalInterval = duration - prevTime
-    val finalZone = heartRateZones.lastOrNull { prevBpm >= it.first } ?: heartRateZones.first()
-
-    val finalZoneIndex = heartRateZones.indexOf(finalZone)
-    if (finalZoneIndex >= 0) {
-        zoneDurations[finalZoneIndex] += finalInterval
-    }
-
-    return zoneDurations.toTypedArray().reversedArray()
+    return timeInZones.reversed()
 }
 
 fun main() {
-    val timesAndBeats = arrayOf(
-        Pair(0, 70),
-        Pair(50, 90),
-        Pair(60, 120),
-        Pair(70, 135),
-        Pair(100, 150),
-        Pair(300, 180),
-        Pair(400, 75)
+    val workout = arrayOf(
+        0 to 70,
+        50 to 90,
+        60 to 120,
+        70 to 135,
+        100 to 150,
+        300 to 180,
+        400 to 75
     )
     val maxHeartRate = 185
-    val duration = 500
-    println(timeInHeartRateZonesForWorkout(timesAndBeats, maxHeartRate, duration).contentDeepToString())
+    val result = timeInHeartRateZonesForWorkout(workout, maxHeartRate)
+    println(result.toList())
 }
 // [100,200,40,160]
