d479f84aa9c44bfcff103ebb706db13bd9e7ef37
gtran6 <gtran6@depaul.edu>
2023-05-11 00:43:51 -0500

    Array

--- src/main/kotlin/array/238.Product-Of-Array-Except-Self.kt
+++ src/main/kotlin/array/238.Product-Of-Array-Except-Self.kt
@@ -4,11 +4,13 @@ fun productExceptSelf(nums: IntArray): IntArray {
     val res = mutableListOf<Int>()
     var prefix = 1
 
+    // Calculate prefix products
     nums.indices.forEach { i ->
         res.add(prefix)
         prefix += nums[i]
     }
 
+    // Calculate suffix products and combine with prefix products
     var postfix = 1
     (nums.size-1 downTo 0).forEach { i ->
         res[i] *= postfix
