dfe8cff2d6457192ea2b7d341777c4c3bb5442fd
gtran6 <gtran6@depaul.edu>
2023-05-11 13:59:48 -0500

    Array

---
+++ src/main/kotlin/array/36.Valid-Sudoku.kt
@@ -0,0 +1,33 @@
+package array
+
+fun isValidSudoku(board: Array<CharArray>): Boolean {
+    val rowSet = Array(9) { mutableSetOf<Char>() }
+    val colSet = Array(9) { mutableSetOf<Char>() }
+    val subBoxSet = Array(9) { mutableSetOf<Char>() }
+
+    for (i in board.indices) {
+        for (j in board.indices) {
+            val digit = board[i][j]
+            if (digit == '.') continue
+            if (!rowSet[i].add(digit)) return false
+            if (!colSet[i].add(digit)) return false
+            val subBoxIndex = (i/3)*3 + (j/3)
+            if (!subBoxSet[subBoxIndex].add(digit)) return false
+        }
+    }
+    return true
+}
+fun main() {
+    val board = arrayOf(
+        charArrayOf('5', '3', '.', '.', '7', '.', '.', '.', '.'),
+        charArrayOf('6', '.', '.', '1', '9', '5', '.', '.', '.'),
+        charArrayOf('.', '9', '8', '.', '.', '.', '.', '6', '.'),
+        charArrayOf('8', '.', '.', '.', '6', '.', '.', '.', '3'),
+        charArrayOf('4', '.', '.', '8', '.', '3', '.', '.', '1'),
+        charArrayOf('7', '.', '.', '.', '2', '.', '.', '.', '6'),
+        charArrayOf('.', '6', '.', '.', '.', '.', '2', '8', '.'),
+        charArrayOf('.', '.', '.', '4', '1', '9', '.', '.', '5'),
+        charArrayOf('.', '.', '.', '.', '8', '.', '.', '7', '9')
+    )
+    println(isValidSudoku(board))
+}
