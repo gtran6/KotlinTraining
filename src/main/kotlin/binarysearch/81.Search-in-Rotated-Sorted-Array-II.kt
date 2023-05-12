76a1c1f5ff5a41af85ae1e5f00a07f4e0e22191c
gtran6 <gtran6@depaul.edu>
2023-05-12 14:08:13 -0500

    Binary Search

--- src/main/kotlin/binarysearch/81.Search-in-Rotated-Sorted-Array-II.kt
+++ src/main/kotlin/binarysearch/81.Search-in-Rotated-Sorted-Array-II.kt
@@ -10,38 +10,33 @@ fun search1(nums: IntArray, target: Int): Boolean {
     var left = 0
     var right = nums.size - 1
 
-    //If the middle element is greater than the first element, then the first half of the array is sorted,
-    // otherwise the second half of the array is sorted.
-    while (left < right) {
-        val mid = left + (right - left) / 2
-        if (nums[mid] > nums[right]) {
-            left = mid + 1
-        } else {
-            right = mid
-        }
-    }
-
-    val pivot = left
-    left = 0
-    right = nums.size - 1
-
-    if (target >= nums[pivot] && target <= nums[right]) {
-        left = pivot
-    } else {
-        right = pivot - 1
-    }
-
     while (left <= right) {
+        // to avoid duplicates
+        while (left < right && nums[left] == nums[left + 1]) ++left
+        while (left < right && nums[right] == nums[right - 1]) --right
+
         val mid = left + (right - left) / 2
 
         if (nums[mid] == target) return true
-        else if (nums[mid] < target) left = mid + 1
-        else right = mid - 1
+
+        if (nums[mid] >= nums[left]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            if (target <= nums[right] && target > nums[mid]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
     }
     return false
 }
 fun main() {
-    val nums = intArrayOf(2,5,6,0,0,1,2)
-    val target = 3
+    val nums = intArrayOf(1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1)
+    val target = 2
     println(search1(nums, target))
 }
---
+++ src/main/kotlin/binarysearch/875.Koko-Eating-Bananas.kt
@@ -0,0 +1,40 @@
+package binarysearch
+
+import kotlin.math.ceil
+
+fun minEatingSpeed(piles: IntArray, h: Int): Int {
+    var left = 1
+    var right = 1
+
+    for (pile in piles) {
+        right = maxOf(right, pile)
+    }
+
+    // uses binary search to find the minimum value of k that satisfies the condition,
+    while (left < right) {
+        val mid = left + (right - left) / 2
+
+        if (canFinish(piles, h, mid)) {
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+}
+//checks whether Koko can finish eating all the bananas within h hours for a given value of k.
+fun canFinish(piles: IntArray, h: Int, speed: Int) : Boolean {
+    var sum = 0
+
+    for (pile in piles) {
+        sum += ceil(pile/speed.toDouble()).toInt()
+
+        if (sum > h) return false
+    }
+    return true
+}
+fun main() {
+    val piles = intArrayOf(30,11,23,4,20)
+    val h = 5
+    println(minEatingSpeed(piles, h))
+}
