cd926945a3dccf14641e02a1070d6288d2456c92
gtran6 <gtran6@depaul.edu>
2023-05-14 22:19:08 -0500

    Two Pointers

---
+++ src/main/kotlin/TwoPointers/844.Backspace-String-Compare.kt
@@ -0,0 +1,39 @@
+package TwoPointers
+
+import java.util.*
+
+fun backspaceCompare1(s: String, t: String): Boolean {
+    return build(s) == build(t)
+}
+fun build(s: String) : String {
+    val stack = Stack<Char>()
+    for (c in s) {
+        if (stack.isNotEmpty() && c == '#') {
+            stack.pop()
+        } else {
+            stack.push(c)
+        }
+    }
+    return stack.toString()
+
+}
+
+fun backspaceCompare(s: String, t: String): Boolean {
+    fun applyBackspaces(str: String): String {
+        val sb = StringBuilder()
+        for (c in str) {
+            if (c == '#') {
+                if (sb.isNotEmpty()) sb.deleteCharAt(sb.length - 1)
+            } else {
+                sb.append(c)
+            }
+        }
+        return sb.toString()
+    }
+    return applyBackspaces(s) == applyBackspaces(t)
+}
+fun main() {
+    val s = "a#c"
+    val t = "b"
+    println(backspaceCompare1(s, t))
+}
